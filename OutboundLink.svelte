a34786522a6f6304e6f24db41f58b5e002155531
Sarah Norris <sarah@sekai.co.uk>
2021-03-22 23:23:53 +0000

    feat: add OutboundLink component

---
+++ OutboundLink.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let href: string = 'javascript:void(0);';
+	export let target: string = undefined;
+	export let rel: string = undefined;
+
+	function trackLink() {		
+		if (typeof window !== 'undefined' && typeof (window as any).gtag !== 'undefined') {
+            (window as any).gtag('event', 'click', {
+                event_category: 'outbound',
+                event_label: href,
+                transport_type: 'beacon',
+            });
+		}
+	}
+</script>
+
+<a
+    {...$$restProps}
+    {href}
+    {target}
+    {rel}
+    on:click={() => { trackLink(); }}
+    on:mouseover
+    on:mouseenter
+    on:mouseout
+    on:focus
+    on:blur
+    on:keydown>
+    <slot />
+</a>
